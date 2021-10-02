e8e66c8f07b111466ea6fcba2bd38a9e6290bcdc
MadakariNayakaHM <mahm20cs@cmrit.ac.in>
2021-10-02 17:04:04 +0000

    first website with html css and bit of JS

---
+++ script.js
@@ -0,0 +1,6 @@
+// alert("I am beginer in web-dev,if it looks bad please adjust");
+
+const madakari =document.createElement("div");
+madakari.classlist.add("container");
+madakari.innerHTML="this is done with JS"
+document.querySelector("#navbar").appendchild(madakari);
